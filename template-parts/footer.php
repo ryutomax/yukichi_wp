4237a233651224a0bfc902c4758e3256bb755c32
Wes-r.sato <r.sato@weshare.co.jp>
2024-08-26 02:11:48 +0000

    add footer icons

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -3,22 +3,25 @@
     </a>
     <footer class="p-footer">
       <div class="p-footer-inner">
-        <h1 class="p-footer-logo">
-          <a class="p-footer-logo-link" href="<?= esc_url(home_url('/')) ?>">
-            <img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/main_logo_white.png" alt="諭吉そば 心躍る、うまいラーメンを" class="p-footer-logo-img" loading="lazy">
-          </a>
-        </h1>
-        <nav class="p-footer-nav">
-          <ul class="p-footer-nav-top u-tb-show-flex">
-            <li class="p-footer-nav-item"><a href="<?= esc_url(home_url('/')) ?>aboutus" class="p-footer-nav-link"><span class="p-footer-nav-i"></a></li>
-            <li class="p-footer-nav-item"><a href="<?= esc_url(home_url('/')) ?>newslist" class="p-footer-nav-link"><span class="p-footer-nav-i"></a></li>
-          </ul>
-          <div class="p-footer-nav-btm">
-            <a href="<?= esc_url(home_url('/')) ?>information" class="p-footer-nav-link">運営会社/利用規約/プライバシーポリシー</a>
-            <a href="<?= esc_url(home_url('/')) ?>contact" class="p-footer-nav-link">/ お問い合わせ</a>
-          </div>
-        </nav>
-        
+          <h1 class="p-footer-logo">
+            <a class="p-footer-logo-link" href="<?= esc_url(home_url('/')) ?>">
+              <img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/main_logo_white.png" alt="諭吉そば 心躍る、うまいラーメンを" class="p-footer-logo-img" loading="lazy">
+            </a>
+          </h1>
+          <ol class="p-footer-icons">
+            <a href="https://www.facebook.com/lapin20150209/" class="p-footer-icon" terget="_blank" rel="noopener noreferrer">
+              <img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/icons/Facebook_icon.png" alt="Facebook 諭吉そば 心躍る、うまいラーメンを" class="p-footer-icon-img">
+            </a>
+            <a href="https://www.instagram.com/?hl=ja" class="p-footer-icon" terget="_blank" rel="noopener noreferrer">
+              <img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/icons/Instagram_icon.png" alt="Instagram 諭吉そば 心躍る、うまいラーメンを" class="p-footer-icon-img">
+            </a>
+            <a href="https://twitter.com/sobacchan_m" class="p-footer-icon" terget="_blank" rel="noopener noreferrer">
+              <img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/icons/X_icon.png" alt="X 諭吉そば 心躍る、うまいラーメンを" class="p-footer-icon-img">
+            </a>
+            <a href="<?= esc_url(home_url('/')) ?>contact" class="p-footer-icon">
+              <img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/icons/contact_icon.png" alt="お問い合わせ" class="p-footer-icon-img">
+            </a>
+          </ol>
       </div>
       <!-- /.l-footer__inner -->
       <small class="p-footer-copyright">Copyright &#169; DH Inc.CO.,LTD. All rights reserved.</small>
