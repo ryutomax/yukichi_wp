3c757dd98732931a7374795462ba539109a78c39
Wes-r.sato <r.sato@weshare.co.jp>
2024-08-27 17:28:44 +0900

    module access

---
+++ template-parts/access.php
@@ -0,0 +1,23 @@
+<section class="p-access c-section fadein-set" id="access">
+  <div class="p-access-inner c-section-inner">
+    <h3 class="p-access-ttl c-title fadein-zoomout-set">
+      <span class="c-title-jp">アクセス</span>
+      <span class="c-title-en">ACCESS</span>
+    </h3>
+    <div class="p-access-cont fadein-set01">
+      <img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/access/access_mv.png" alt="諭吉そば" class="p-access-img">
+      <div class="p-access-info">
+        <div class="p-access-map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.7479746645863!2d139.7454329!3d35.6585805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bbd9009ec09%3A0x481a93f0d2a409dd!2z5p2x5Lqs44K_44Ov44O8!5e0!3m2!1sja!2sjp!4v1723253211003!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <p class="p-access-meta">
+          千葉県松戸市小根本51-9 マツドKビル 1F <br>
+          営業時間：11:45～14:00 <br>
+          定休日  ：土曜日、日曜日、祝日
+        </p>
+      </div>
+    </div>
+    <!-- /.p-access-cont -->
+  </div>
+  <!-- /.p-menu-inner -->
+</section>
