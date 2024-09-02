8e84efc2d2b320ccc7cc400dfa448785288deb22
wes-r.sato <r.sato@weshare.co.jp>
2024-09-02 15:37:59 +0900

    fixed googlemap

--- template-parts/access.php
+++ template-parts/access.php
@@ -8,7 +8,7 @@
       <img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/access/access_mv.jpg" alt="諭吉そば" class="p-access-img">
       <div class="p-access-info">
         <div class="p-access-map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.7479746645863!2d139.7454329!3d35.6585805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bbd9009ec09%3A0x481a93f0d2a409dd!2z5p2x5Lqs44K_44Ov44O8!5e0!3m2!1sja!2sjp!4v1723253211003!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6473.043770290276!2d139.90194637422053!3d35.787119224152555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601884faca50725d%3A0x7d74f883fd2378c8!2z44CSMjcxLTAwNzMg5Y2D6JGJ55yM5p2-5oi45biC5bCP5qC55pys77yV77yR4oiS77yZIOODnuODg-ODiUvjg5Pjg6sgMWY!5e0!3m2!1sja!2sjp!4v1725258756629!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <p class="p-access-meta">
           千葉県松戸市小根本51-9 マツドKビル 1F <br>
