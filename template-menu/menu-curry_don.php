8ca001d7e7c21eb14543fd527c3bd3321ae36169
wes-r.sato <r.sato@weshare.co.jp>
2024-09-03 18:06:09 +0900

    fixed

--- template-menu/menu-curry_don.php
+++ template-menu/menu-curry_don.php
@@ -24,7 +24,11 @@
 				$wp_query->the_post();
 	?>
 			<li class="p-menu-item fadein-set01">
-				<img class="p-menu-item-img" src="<?php the_field('メニュー画像'); ?>" alt="<?php the_field('メニュー名'); ?>">
+				<?php if( get_field('メニュー画像') ): ?>
+					<img class="p-menu-item-img" src="<?php the_field('メニュー画像'); ?>" alt="<?php the_field('メニュー名'); ?>">
+				<?php else: ?>
+					<img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/menu/menu_ready.png" alt="<?php the_field('メニュー名'); ?>" class="p-menu-item-img">
+					<?php endif; ?>				
 				<h4 class="p-menu-name">
 					<span class="sub"><?php the_field('メニューリード文'); ?></span>
 					<span class="main"><?php the_field('メニュー名'); ?></span>
