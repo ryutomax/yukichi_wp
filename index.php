e18d558233f00aff1a5952f582a1a12742856481
ryutomax <maxy42sr@gmail.com>
2024-09-09 00:22:32 +0900

    add mv sample

--- index.php
+++ index.php
@@ -3,50 +3,55 @@
 	get_template_part('template-parts/header');
   require_once(locate_template('template-parts/module_func.php', true, true));
 ?>
+
 <main class="l-main">
-  <section class="p-top c-section">
-    <?php get_template_part('template-parts/top-header'); ?>
-    <div class="u-sp-show">
-      <?php get_template_part('template-parts/banner'); ?>
-    </div>
-    <div class="steamWrap fadein-set01">
-      <div class="steamBox">
-        <img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/greeting/greeting_mv.jpg" alt="心躍る、うまい一杯を" class="p-top-main-img">
-        <div class="steam01 c-steam"><img class="steam01-img" src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/common/steam01.png" alt="湯気"></div>
-        <div class="steam02 c-steam"><img class="steam02-img" src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/common/steam02.png" alt="湯気"></div>
-        <div class="steam03 c-steam"><img class="steam03-img" src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/common/steam01.png" alt="湯気"></div>
-        <div class="steam04 c-steam"><img class="steam04-img" src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/common/steam02.png" alt="湯気"></div>
-        <div class="steam05 c-steam"><img class="steam05-img" src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/common/steam01.png" alt="湯気"></div>
+  <section class="p-cover p-cover"></section>
+  <section class="p-top c-section p-fixBox" id="a01">
+    <!-- <div class="p-fixBox-inner"> -->
+      <?php get_template_part('template-parts/top-header'); ?>
+      <div class="u-sp-show">
+        <?php get_template_part('template-parts/banner'); ?>
       </div>
-    </div>
-    <div class="p-top-inner c-section-inner">
-      <div class="p-top-main" id="introduction">
-        <div class="p-top-main-cont">
-          <h2 class="p-top-main-copy fadein-zoomout-set"><em>心躍る、</em><em>うまい</em><em>一杯を</em></h2>
-          <div class="p-top-main-text">
-            <p class="p-top-main-text-jp fadein-set02">
-              <em>東京蒲田の老舗蕎麦店で長年修行を重ねた後、<br>
-              ここ松戸の地でおよそ10年にわたり蕎麦店を営んできた、<br>
-              74歳松戸名物おばちゃん店主の店が<br>
-              心機一転『諭吉そば』として生まれ変わりました。</em><br><br>
-              <em>74歳にしてとにかく元気な店主が<br>
-              「まかない」をベースに考案した<br>
-              特製醤油ラーメンをメインに、<br>
-              極太蕎麦やうどん、揚げたての天ぷらや<br>
-              蕎麦店時代から人気の手作り唐揚げなど、<br>
-              毎日店主自ら仕込んでいます。<br>
-              美味しくてお腹も満足していただけるボリューム感と<br>
-              アットホームな雰囲気、店主の心意気は<br>
-              蕎麦店時代から変わらず引き継がれたもの。<br>
-              カウンター８席の小さなお店ではありますが、<br>
-              皆様のお越しをお待ちしております。</em>
-            </p>
-          </div>
+      <div class="steamWrap fadein-set01">
+        <div class="steamBox">
+          <img src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/greeting/greeting_mv.jpg" alt="心躍る、うまい一杯を" class="p-top-main-img">
+          <div class="steam01 c-steam"><img class="steam01-img" src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/common/steam01.png" alt="湯気"></div>
+          <div class="steam02 c-steam"><img class="steam02-img" src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/common/steam02.png" alt="湯気"></div>
+          <div class="steam03 c-steam"><img class="steam03-img" src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/common/steam01.png" alt="湯気"></div>
+          <div class="steam04 c-steam"><img class="steam04-img" src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/common/steam02.png" alt="湯気"></div>
+          <div class="steam05 c-steam"><img class="steam05-img" src="<?php echo esc_url(get_template_directory_uri() . '/'); ?>assets/images/common/steam01.png" alt="湯気"></div>
         </div>
       </div>
-      <!-- /.p-top-main -->
-    </div>
-    <!-- /.p-top-inner -->
+      <div class="p-top-inner c-section-inner">
+        <div class="p-top-main" id="introduction">
+          <div class="p-top-main-cont">
+            <h2 class="p-top-main-copy fadein-zoomout-set"><em>心躍る、</em><em>うまい</em><em>一杯を</em></h2>
+            <div class="p-top-main-text">
+              <p class="p-top-main-text-jp fadein-set02">
+                <em>東京蒲田の老舗蕎麦店で長年修行を重ねた後、<br>
+                ここ松戸の地でおよそ10年にわたり蕎麦店を営んできた、<br>
+                74歳松戸名物おばちゃん店主の店が<br>
+                心機一転『諭吉そば』として生まれ変わりました。</em><br><br>
+                <em>74歳にしてとにかく元気な店主が<br>
+                「まかない」をベースに考案した<br>
+                特製醤油ラーメンをメインに、<br>
+                極太蕎麦やうどん、揚げたての天ぷらや<br>
+                蕎麦店時代から人気の手作り唐揚げなど、<br>
+                毎日店主自ら仕込んでいます。<br>
+                美味しくてお腹も満足していただけるボリューム感と<br>
+                アットホームな雰囲気、店主の心意気は<br>
+                蕎麦店時代から変わらず引き継がれたもの。<br>
+                カウンター８席の小さなお店ではありますが、<br>
+                皆様のお越しをお待ちしております。</em>
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- /.p-top-main -->
+      </div>
+      <!-- /.p-top-inner -->
+    <!-- </div> -->
+    <!-- /.p-fixBox-inner -->
   </section>
   <section class="p-news c-section" id="news">
     <h3 class="p-news-ttl c-title fadein-zoomout-set">
@@ -57,19 +62,19 @@
     <div class="p-news-inner c-section-inner">
       <div class="p-news-list">
         <?php
-					$args = array(
-						'post_type' => 'news',
-						'post_status' => 'publish',
+          $args = array(
+            'post_type' => 'news',
+            'post_status' => 'publish',
             'posts_per_page' => 10,
-					);
+          );
           $news_count = 1;
           // 表示される記事数取得
-					$wp_query = new WP_Query( $args );
+          $wp_query = new WP_Query( $args );
           $news_num = $wp_query->found_posts;
-					if ( $wp_query->have_posts() ):
-					while ( $wp_query->have_posts() ):
-						$wp_query->the_post();
-				?>
+          if ( $wp_query->have_posts() ):
+          while ( $wp_query->have_posts() ):
+            $wp_query->the_post();
+        ?>
           <?php if($news_count <= 5): ?>
             <article class="p-news-itme fadein-set01">
               <a href="<?php the_permalink(); ?>" class="p-news-link">
